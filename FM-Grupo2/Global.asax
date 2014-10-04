31c0df0f90477d353e7f8ba4decefb9c4fda7b0d
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-04 10:36:06 +0100

    Projeto em estado default

---
+++ FM-Grupo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FM_Grupo2.MvcApplication" Language="C#" %>
---
+++ FM-Grupo2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FM_Grupo2.Startup))]
+namespace FM_Grupo2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
