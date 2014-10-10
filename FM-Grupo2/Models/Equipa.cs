087eab0a67675235f67eec8edda97b286ba343bf
JoseMariaAndrade <jmariaandrade131@gmail.com>
2014-10-10 19:18:28 +0100

    Nuget Update e Modelo Equipa

---
+++ FM-Grupo2/Models/Equipa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FM_Grupo2.Models
+{
+    public class Equipa
+    {
+        public int ID { get; set; }
+    }
+}
