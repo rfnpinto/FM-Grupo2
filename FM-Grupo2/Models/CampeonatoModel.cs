5843100ba45a23840aae8da7dd1f61272b0a0527
JoseMariaAndrade <jmariaandrade131@gmail.com>
2014-10-10 21:58:45 +0100

    Campeonato Modelo

--- FM-Grupo2/Models/CampeonatoModel.cs
+++ FM-Grupo2/Models/CampeonatoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,20 +10,30 @@ namespace FM_Grupo2.Models
     {
         public int ID;
 
+        [Required]
+        [StringLength(50, ErrorMessage = "O Nome do Campeonato não pode execeder os 50 caracteres")]
         public string Nome;
         
         public string LogotipoPath;
         
+        [Required]
         public char Abranjencia;
 
+        [Required]
         public char Frequancia;
 
+        [Required]
         public string Nacionalidade;
 
+        [Required]
         public int EntidadeOrganizadora;
 
+        [Required]
+        [StringLength(200, ErrorMessage = "A Descrição do Campeonato não pode execeder os 200 caracteres")]
         public string Descricao;
 
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataFundacao;
     }
 }
