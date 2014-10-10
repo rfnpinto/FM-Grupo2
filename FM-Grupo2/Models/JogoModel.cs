3ab22030f9cf1dbff5e8a5d64c12d07363f24b4a
xaviercampos <xavier_xcc@hotmail.com>
2014-10-10 21:49:16 +0100

    Modelo Jogo

--- FM-Grupo2/Models/JogoModel.cs
+++ FM-Grupo2/Models/JogoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,14 +11,20 @@ namespace FM_Grupo2.Models
 
         public int ID { get; set; }
 
+        [Required]
         public string EquipaVisitante { get; set; }
 
+        [Required]
         public string EquipaVisitada { get; set; }
 
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Data { get; set; }
 
+        [Required]
         public DateTime Hora { get; set; }
 
+        [Required]
         public string Resultado { get; set; }
     }
     
