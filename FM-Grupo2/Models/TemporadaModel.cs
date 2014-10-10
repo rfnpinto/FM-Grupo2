d55cab6a9343fe442e1ccf80528cc4dc3adab66a
xaviercampos <xavier_xcc@hotmail.com>
2014-10-10 19:47:13 +0100

    Model Temporada

---
+++ FM-Grupo2/Models/TemporadaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FM_Grupo2.Models
+{
+    public class TemporadaModel
+    {
+
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public DateTime Ano { get; set; }
+        public string Descricao { get; set; }
+        public int NEquipas { get; set; }
+        public int NJornadas { get; set; }
+
+
+     
+
+    }
+}
