536bbec363579fe0e12e9c851f4ec7dc74dfe802
xaviercampos <xavier_xcc@hotmail.com>
2014-10-10 21:13:38 +0100

    Modelo Jogo

---
+++ FM-Grupo2/Models/JogoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FM_Grupo2.Models
+{
+    public class JogoModel
+    {
+
+        public int ID { get; set; }
+
+        public string EquipaVisitante { get; set; }
+
+        public string EquipaVisitada { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public DateTime Hora { get; set; }
+
+        public string Resultado { get; set; }
+    }
+    
+}
