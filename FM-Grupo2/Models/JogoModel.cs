71e8dc834f0220965978dff265ec3280aee764b5
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-22 21:57:18 +0100

    estrutura modelo jogo

--- FM-Grupo2/Models/JogoModel.cs
+++ FM-Grupo2/Models/JogoModel.cs
@@ -8,23 +8,26 @@ namespace FM_Grupo2.Models
 {
     public class JogoModel
     {
-
+        /**
+         * ID's;
+         */
         public int ID { get; set; }
 
-        [Required]
-        public string EquipaVisitante { get; set; }
-
-        [Required]
-        public string EquipaVisitada { get; set; }
+        /**
+         * Campos do modelo;
+         */
+        public int EquipaVisitada { get; set; }
 
         [Required]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Data { get; set; }
 
-        [Required]
-        public DateTime Hora { get; set; }
-
         public string Resultado { get; set; }
+
+        /**
+         * Ligações com outros modelos;
+         */
+        public virtual ICollection<EquipaModel> Equipa { get; set; }
     }
     
 }
