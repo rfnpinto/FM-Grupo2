0a4be8a19df908be7a65943e66302d3dac569946
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-22 21:27:58 +0100

    Estrutura temporada model

--- FM-Grupo2/Models/TemporadaModel.cs
+++ FM-Grupo2/Models/TemporadaModel.cs
@@ -9,25 +9,32 @@ namespace FM_Grupo2.Models
     public class TemporadaModel
     {
 
-        public int ID { get; set; }
+        /**
+         * ID's;
+         */
+        public int TemporadaID { get; set; }
 
-        [Required]
-        public virtual CampeonatoModel Campeonato { get; set; }
-        
+        public int CampeonatoID { get; set; }
+
+        /**
+         * Campos do Modelo;
+         */
         [Required]
         public string Nome { get; set; }
         
         [Required]
-        [DisplayFormat(DataFormatString = "{0:yyyy/yyyy+1}", ApplyFormatInEditMode = true)]
         public DateTime Ano { get; set; }
         
         [Required]
         public string Descricao { get; set; }
-        
 
-        public virtual ICollection<EquipaModel> Equipas { get; set; }
-        
         public int NJornadas { get; set; }
+
+        /**
+         * Ligações aos outros modelos;
+         */
+        public virtual CampeonatoModel Campeonato { get; set; }
+        public virtual ICollection<EquipaModel> Equipas { get; set; }
 
     }
 }
