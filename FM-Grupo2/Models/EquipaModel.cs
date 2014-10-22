ffa1ff911faea00ff18b9c402c57ee9e90b5dd22
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-22 21:36:21 +0100

    Estrutura modelo Equipa

--- FM-Grupo2/Models/EquipaModel.cs
+++ FM-Grupo2/Models/EquipaModel.cs
@@ -8,13 +8,21 @@ namespace FM_Grupo2.Models
 {
     public class EquipaModel
     {
-        public int ID { get; set; }
+        /**
+         * ID's:
+         */
+        public int EquipaID { get; set; }
 
-        public string Logotipo { get; set; }
-
+        /**
+         * Campos do Modelo;
+         */
         [Required]
         [StringLength(50, ErrorMessage = "O Nome da Equipa não pode execeder os 50 caracteres")]
         public string NomeEquipa { get; set; }
+
+        public string Logotipo { get; set; }
+
+        public string Clube { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime DataFundacao { get; set; }
